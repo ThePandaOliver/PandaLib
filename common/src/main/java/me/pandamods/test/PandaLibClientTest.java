50aba22c960ab60b7d7ff0a04e32125848fb415f
The Panda Oliver <Oliver.h.froberg@gmail.com>
2024-06-10 01:17:23 +0200

    Remove client-specific classes and references
    The commit eliminates client-specific classes from the codebase, simplifying the structure and reducing potential points of failure. In the context of PandaLib, this refactoring won't impact its main functionality. It should reduce complexity, make code cleaner and easier to maintain.

--- common/src/main/java/me/pandamods/test/PandaLibClientTest.java
+++
@@ -1,9 +0,0 @@
-package me.pandamods.test;
-
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
-
-@Environment(EnvType.CLIENT)
-public class PandaLibClientTest {
-	public static void init() {}
-}
--- neoforge/src/main/java/me/pandamods/pandalib/neoforge/PandaLibNeoForge.java
+++ neoforge/src/main/java/me/pandamods/pandalib/neoforge/PandaLibNeoForge.java
@@ -2,16 +2,19 @@ package me.pandamods.pandalib.neoforge;
 
 import dev.architectury.platform.hooks.EventBusesHooks;
 import me.pandamods.pandalib.PandaLib;
+import me.pandamods.pandalib.api.client.screen.config.menu.ConfigScreenProvider;
+import me.pandamods.test.PandaLibTest;
+import net.neoforged.fml.ModLoadingContext;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLEnvironment;
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 
 @Mod(PandaLib.MOD_ID)
 public class PandaLibNeoForge {
     public PandaLibNeoForge() {
-//		EventBuses.registerModEventBus(PandaLib.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 		PandaLib.init();
-		if (FMLEnvironment.dist.isClient()) {
-			PandaLibNeoForgeClient.clientInit();
-		}
+		ModLoadingContext.get().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class, () ->
+				new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) ->
+						new ConfigScreenProvider<>(screen, PandaLibTest.CLIENT_CONFIG).get()));
     }
 }

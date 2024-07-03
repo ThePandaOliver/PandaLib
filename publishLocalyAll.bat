3f638ffb01c0736c9b185bfa1203d31d7d3ac956
Panda Mods <Oliver.h.froberg@gmail.com>
2024-07-03 18:06:54 +0200

    Upgrade architectury.loom version
    The architectury.loom version was updated from 1.6-SNAPSHOT to 1.7-SNAPSHOT in the build.gradle file. This change ensures the codebase is using the most recent stable version of the architectury.loom plugin.

---
+++ publishLocalyAll.bat
@@ -0,0 +1,15 @@
+@echo off & setlocal enabledelayedexpansion
+
+echo ==================== Cleaning workspace to build ====================
+    call .\gradlew.bat clean --no-daemon
+
+@rem Loop trough everything in the version properties folder
+for %%f in (versionProperties\*) do (
+    @rem Get the name of the version that is going to be compiled
+    set version=%%~nf
+
+    echo ==================== Building !version! ====================
+    call .\gradlew.bat publishToMavenLocal -PmcVer="!version!" --no-daemon
+)
+
+endlocal

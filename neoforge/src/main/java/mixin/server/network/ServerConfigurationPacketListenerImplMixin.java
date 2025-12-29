0f4535e0f3824be46ec7daa2ba2d56357246144a
The Panda Oliver <oliver.froberg@proton.me>
2025-12-30 00:44:16 +0100

    Style fix: Adjust annotation formatting and spacing in `ServerConfigurationPacketListenerImplMixin`

--- neoforge/src/main/java/mixin/server/network/ServerConfigurationPacketListenerImplMixin.java
+++ neoforge/src/main/java/mixin/server/network/ServerConfigurationPacketListenerImplMixin.java
@@ -19,6 +19,7 @@ import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.network.ServerCommonPacketListenerImpl;
 import net.minecraft.server.network.ServerConfigurationPacketListenerImpl;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;

293fd5a7065fb539204672926b72957934b8222d
The Panda Oliver <oliver.froberg@proton.me>
2025-12-30 00:19:44 +0100

    Refactor: Replace `DimensionTransition` with `ServerLevel` and `Entity` in method parameters

--- src/main/kotlin/mixin/server/level/ServerPlayerKtImpl.kt
+++ src/main/kotlin/mixin/server/level/ServerPlayerKtImpl.kt
@@ -21,7 +21,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable
 
 object ServerPlayerKtImpl {
 	fun onDimensionChangePreEvent(player: ServerPlayer, destination: ServerLevel, cir: CallbackInfoReturnable<Entity>) {
-		val cancelled = !serverPlayerChangeDimensionPreEvent.invoker(player, player.level() as ServerLevel, destination)
+		val cancelled = !serverPlayerChangeDimensionPreEvent.invoker(
+			player, player.level() as ServerLevel, destination)
 		if (cancelled) cir.returnValue = null
 	}
 

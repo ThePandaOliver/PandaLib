7c8289378b90a9d5e4ca516d4c4938ef981c6719
Oliver Froberg <Oliver.h.froberg@gmail.com>
2026-09-13 13:03:28 +0200

    Removed old code

--- src/main/kotlin/dev/pandasystems/pandalib/config/exceptions/ConfigNotRegisteredException.kt
+++
@@ -1,7 +0,0 @@
-package dev.pandasystems.pandalib.config.exceptions
-
-import net.minecraft.resources.Identifier
-
-class ConfigNotRegisteredException(resourceLocation: Identifier) : RuntimeException() {
-	override val message: String = "Config $resourceLocation is not registered"
-}
